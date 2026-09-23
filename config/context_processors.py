ddc3be50101d6b38aa0993d4626dedac20e19216
Zn070515 <302025510036@zjut.edu.cn>
2026-09-23 20:53:12 +0800

    fix: type commercial branding context

--- config/context_processors.py
+++ config/context_processors.py
@@ -1,7 +1,8 @@
 from django.conf import settings
+from django.http import HttpRequest
 
 
-def artflow_branding(_request):
+def artflow_branding(_request: HttpRequest) -> dict[str, str]:
     return {
         "artflow_organization_name": settings.ARTFLOW_ORGANIZATION_NAME,
     }
